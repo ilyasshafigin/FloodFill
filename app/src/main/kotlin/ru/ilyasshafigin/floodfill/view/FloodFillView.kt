142016d97db85144dfe13a6345e035df9882201a
Ilyas Shafigin <ilyasshafigin@gmail.com>
2020-02-02 19:17:18 +0300

    Create project

---
+++ app/src/main/kotlin/ru/ilyasshafigin/floodfill/view/FloodFillView.kt
@@ -0,0 +1,20 @@
+package ru.ilyasshafigin.floodfill.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class FloodFillView : View {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+        super(context, attrs, defStyleAttr)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
+        super(context, attrs, defStyleAttr, defStyleRes)
+
+
+}
